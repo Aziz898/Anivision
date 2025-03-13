68e9e37053f793d14bd453673e2e38e743155b7f
Aziz898 <90274448+Aziz898@users.noreply.github.com>
2025-03-13 23:25:32 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Anivision UI Loaded");
+  console.log("Anivision UI Loaded in Russian");
   // Здесь можно добавить интерактивность (меню, переходы, модальные окна и т.д.)
 });
