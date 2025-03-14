fba6633e0fffef331528dd64ebcc3439bd83a28e
Aziz898 <90274448+Aziz898@users.noreply.github.com>
2025-03-14 02:07:17 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("Anivision MiniApp loaded with minimal black-purple design");
+  console.log("Anivision MiniApp with splash screen");
+
+  const splashScreen = document.getElementById('splash-screen');
+
+  // Имитация, что через 2 сек splash пропадает
+  setTimeout(() => {
+    // Добавим анимацию "fadeOut" (создана, но не применена) - можно сделать и по-другому
+    splashScreen.style.animation = "fadeOut 0.7s forwards";
+    // Удалим splash через 0.7s
+    setTimeout(() => {
+      splashScreen.style.display = "none";
+    }, 700);
+  }, 2000);
 
   // Пример: обработка клика по меню
   const menuBtn = document.querySelector(".menu-btn");
